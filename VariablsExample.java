df674114067f61f4ceab7c6f7dba74b4708a8d60
prajina2000 <prajinavkrishnan2000@gmail.com>
2024-10-07 10:09:05 +0530

    java variables

---
+++ VariablsExample.java
@@ -0,0 +1,28 @@
+public class VariablsExample {
+    public static void main(String[] args) {
+        
+        int age = 25;
+        int year = 2024;
+        int birthYear = 1999;
+        int houseNumber = 101;
+        int pinCode = 671351;
+
+        
+        String firstName = "mariya";
+        String lastName = "jacob";
+        String city = "Bengaluru";
+        String country = "India";
+        String jobTitle = "Software Engineer";
+
+        
+        System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("Age: " + age);
+        System.out.println("Birth Year: " + birthYear);
+        System.out.println("Year: " + year);
+        System.out.println("House Number: " + houseNumber);
+        System.out.println("City: " + city);
+        System.out.println("Country: " + country);
+        System.out.println("Job Title: " + jobTitle);
+        System.out.println("pin Code: " + pinCode);
+    }
+}
