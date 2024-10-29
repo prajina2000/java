b00959243937cd147f60d29cf78c1459449888d3
prajina2000 <prajinavkrishnan2000@gmail.com>
2024-10-29 10:06:37 +0530

    constructor chaining

---
+++ CustomData.java
@@ -0,0 +1,7 @@
+public class CustomData{
+	public static void main(String[] args){
+		 Customer customer=new Customer("manu",9876578,"btm_layout",6754,"pencil");
+		 customer.printDetails();
+
+	}
+}
